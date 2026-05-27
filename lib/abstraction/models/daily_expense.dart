c137497ebff88f54e717abbcb3999f52990c2e02
ngxdcuong <ngxdcuong@gmail.com>
2026-05-27 15:48:50 +0700

    Delete lib/abstraction/models directory

--- lib/abstraction/models/daily_expense.dart
+++
@@ -1,6 +0,0 @@
-class DailyExpense {
-  final String day;
-  final double amount;
-
-  DailyExpense({required this.day, required this.amount});
-}
--- lib/abstraction/models/monthly_expense.dart
+++
@@ -1,5 +0,0 @@
-class MonthlyExpense {
-  final String month;
-  final double amount;
-  MonthlyExpense({required this.month, required this.amount});
-}
